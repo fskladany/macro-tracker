148778317fb1a13cbea249e84f42344c60de373a
Frantisek Skladany <fr.skladany@gmail.com>
2025-10-06 16:52:50 +0200

    Disable textarea dragging event

--- fluency.js
+++ fluency.js
@@ -29,7 +29,7 @@
                }
                
                if (
-                    tagName === 'button' ||
+                    tagName === 'button' || tagName === 'textarea' ||
                     (tagName === 'input' && (type === 'text' || type === 'number')) ||
                     tagName === 'select'
                ) {
